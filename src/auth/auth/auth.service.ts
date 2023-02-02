e2cd731f015dcf000a93a8d5591af0a34f4fedb5
Tomasz Smoczyński <tomeksmo@gmail.com>
2023-02-02 21:53:40 +0100

    dodanie cors na localhost i credentials

--- src/auth/auth/auth.service.ts
+++ src/auth/auth/auth.service.ts
@@ -34,7 +34,6 @@ export class AuthService {
     if (!validPassword) {
       throw new BadRequestException('Wrong credentials');
     }
-
     return user;
   }
 
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { DatabaseExceptionFilter } from './filters/database-exception.filters';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    origin: 'http://localhost:4200',
+  });
   app.setGlobalPrefix('api/v1');
   app.use(cookieParser());
   //whitelist — removes any property of query, body, and a parameter that is not part of our DTO
